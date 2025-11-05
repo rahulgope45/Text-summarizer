59198099d49e59ec6eb566c7797596a246b28c10
rahulgope45 <rahuleile456@gmail.com>
2025-11-05 07:38:08 +0530

    link changes

--- frontend/src/Services/config.js
+++ frontend/src/Services/config.js
@@ -1,10 +1,10 @@
 export const AUTH_BASE_URL = 
 import.meta.env.MODE === "production"
  ? ""
- : "https://text-summarizer-1-gadp.onrender.com/api/auth";
+ : "https://text-summarizer-1-gadp.onrender/api/auth";
 
 
  export const SUMMARY_BASE_URL =
  import.meta.env.MODE === "production"
- ? "https://text-summarizer-1-gadp.onrender.com/api"
+ ? "https://text-summarizer-1-gadp.onrender/api"
  : "http://localhost:3002/api"
